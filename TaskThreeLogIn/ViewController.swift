6f6d5045c8ecd00de2920ccb90dc7ea6e35c47a9
Alex Sobolev <31108@mail.ru>
2022-05-26 20:23:10 +0300

    Setup alert controller

--- TaskThreeLogIn/ViewController.swift
+++ TaskThreeLogIn/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextFiled: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
     }
 
-
+    @IBAction func logInButtonPressed() {
+    }
+    
+    @IBAction func forgetUserNameButtonPressed() {
+    }
+    @IBAction func forgetPassordButtonPressed() {
+    }
 }
 
+// MARK: - Private Methods
+extension ViewController {
+    private func showAlert(with title: String, and massage: String) {
+        let alert = UIAlertController(title: title, message: massage, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.passwordTextFiled.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
