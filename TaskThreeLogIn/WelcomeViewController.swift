eb47c2b306e2d10ad12e3ee824b09dbddcea40d3
Alex Sobolev <31108@mail.ru>
2022-05-27 11:50:01 +0300

    Create segue with WelcomeViewController

---
+++ TaskThreeLogIn/WelcomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeViewController.swift
+//  TaskThreeLogIn
+//
+//  Created by Александр Соболев on 27.05.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
