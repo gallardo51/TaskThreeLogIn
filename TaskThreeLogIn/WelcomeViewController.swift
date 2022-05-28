926be59e564b906c83b777106257b6e6bc5abfaf
Alex Sobolev <31108@mail.ru>
2022-05-28 17:51:14 +0300

    Setup welcomeVC text

--- TaskThreeLogIn/WelcomeViewController.swift
+++ TaskThreeLogIn/WelcomeViewController.swift
@@ -9,8 +9,13 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    var user = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        welcomeLabel.text = "Welcome, \(user)!"
 
         
     }
