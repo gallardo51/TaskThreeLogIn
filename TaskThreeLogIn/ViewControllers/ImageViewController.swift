fc62148c58d5897d7cb0b3baa569e00ebbf7216e
Alex Sobolev <31108@mail.ru>
2022-06-12 23:21:29 +0300

    Setup UIColor ImageViewController

--- TaskThreeLogIn/ViewControllers/ImageViewController.swift
+++ TaskThreeLogIn/ViewControllers/ImageViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ImageViewController: UIViewController {
     
+    private let primaryColor = UIColor(
+        red: 210/255,
+        green: 109/255,
+        blue: 128/255,
+        alpha: 1)
+    private let secondaryColor = UIColor(
+        red: 110/255,
+        green: 180/255,
+        blue: 150/255,
+        alpha: 1)
+    
     var user: User!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
     }
     
 
